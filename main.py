8c4e917c720b4f807f08e36e9e2757ca74597811
Firge <59466895+Firge@users.noreply.github.com>
2021-03-24 16:07:01 +1000

    Update main.py
    map function update

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import pygame
 import math
 
 
-def image_by_coords(coords, **kwargs):
+def image_by_coords(coords, l, **kwargs):
     map_server = 'https://static-maps.yandex.ru/1.x/'
     map_params = {
-        "l": 'map',
+        "l": l,
         "ll": ','.join(coords)}
     map_params.update(kwargs)
     response = requests.get(map_server, params=map_params)
@@ -28,7 +28,7 @@ sizes = ["90,90", "50,50", "40,40", "20,20", "10,10", "5,5",
 delta = 9
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-image = image_by_coords([str(x) for x in coords], spn=sizes[delta])
+image = image_by_coords([str(x) for x in coords], 'map', spn=sizes[delta])
 running = True
 while running:
     for event in pygame.event.get():
@@ -37,33 +37,33 @@ while running:
                 if delta == 18:
                     continue
                 delta += 1
-                image = image_by_coords([str(x) for x in coords], spn=sizes[delta])
+                image = image_by_coords([str(x) for x in coords], 'map', spn=sizes[delta])
             elif event.key == pygame.K_PAGEDOWN:
                 if delta == 0:
                     continue
                 delta -= 1
-                image = image_by_coords([str(x) for x in coords], spn=sizes[delta])
+                image = image_by_coords([str(x) for x in coords], 'map', spn=sizes[delta])
             elif event.key == pygame.K_UP:
                 dlt = float(sizes[delta].split(',')[0])
                 if coords[1] + dlt >= 90 - dlt:
                     continue
                 coords = (coords[0], coords[1] + dlt)
                 print(coords)
-                image = image_by_coords([str(x) for x in coords], spn=sizes[delta])
+                image = image_by_coords([str(x) for x in coords], 'map', spn=sizes[delta])
             elif event.key == pygame.K_DOWN:
                 dlt = float(sizes[delta].split(',')[0])
                 if coords[1] - dlt <= -90 + dlt:
                     continue
                 coords = (coords[0], coords[1] - dlt)
-                image = image_by_coords([str(x) for x in coords], spn=sizes[delta])
+                image = image_by_coords([str(x) for x in coords], 'map', spn=sizes[delta])
             elif event.key == pygame.K_LEFT:
                 dlt = float(sizes[delta].split(',')[0])
                 coords = ((coords[0] - dlt * 1.5 + 180) % 360 - 180, coords[1])
-                image = image_by_coords([str(x) for x in coords], spn=sizes[delta])
+                image = image_by_coords([str(x) for x in coords], 'map', spn=sizes[delta])
             elif event.key == pygame.K_RIGHT:
                 dlt = float(sizes[delta].split(',')[0])
                 coords = ((coords[0] + dlt * 1.5 + 180) % 360 - 180, coords[1])
-                image = image_by_coords([str(x) for x in coords], spn=sizes[delta])
+                image = image_by_coords([str(x) for x in coords], 'map', spn=sizes[delta])
         elif event.type == pygame.QUIT:
             running = False
     screen.blit(image, (0, 0))
